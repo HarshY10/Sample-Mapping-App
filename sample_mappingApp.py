cc89762b25fa27700e0b81ee8b224b9a3c083532
HarshY10 <131291758+HarshY10@users.noreply.github.com>
2024-05-28 14:50:49 +0530

    Update sample_mappingApp.py

--- sample_mappingApp.py
+++ sample_mappingApp.py
@@ -3,11 +3,11 @@ import folium
 from streamlit_folium import st_folium
 
 #App Title
-st.header(':blue[Address Mapping App] :world_map:', divider='rainbow')
+st.header(':blue[Address Mapped App] :world_map:', divider='rainbow')
 st.write("A Demonstration app developed by Harsh Y for the CODE HR Summit.")
+st.write("Click on the marker to view popup!")
 def_loc = [12.9915, 80.2337] #default IIT Madras 12.9915° N, 80.2337° E
-def_map = folium.Map(location = def_loc, zoom_start=12) 
-
+def_map = folium.Map(location = def_loc, zoom_start=20) 
 #Setting the marker
 #Using HTML Tags to enlarge text and the width of popup box to fit text
 folium.Marker([12.9915, 80.2337], popup=folium.Popup("<h5>IIT Madras</h5>", max_width=200)).add_to(def_map) 
